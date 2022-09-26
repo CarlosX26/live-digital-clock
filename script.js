345931155d94ae52aa3143caf0fd4fcb932bcaf7
Carlos Junio <cjunior1@live.com>
2022-09-26 08:34:17 -0300

    fix: bugs in date

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
+function getTime() {
+  const date = new Date();
+  const boxHours = document.getElementById("hours");
+  const boxMinutes = document.getElementById("minutes");
+  const boxSeconds = document.getElementById("seconds");
+  const currentDate = document.getElementById("date");
 
+  const hours =
+    `${date.getHours()}`.length < 2 ? `0${date.getHours()}` : date.getHours();
+  const minutes =
+    `${date.getMinutes()}`.length < 2
+      ? `0${date.getMinutes()}`
+      : date.getMinutes();
+  const seconds =
+    `${date.getSeconds()}`.length < 2
+      ? `0${date.getSeconds()}`
+      : date.getSeconds();
 
-function getTime(){
+  boxHours.innerText = hours;
+  boxMinutes.innerText = minutes;
+  boxSeconds.innerText = seconds;
 
-    const date            = new Date()
-    const boxHours        = document.getElementById("hours")
-    const boxMinutes      = document.getElementById("minutes")
-    const boxSeconds      = document.getElementById("seconds")
-    const currentDate     = document.getElementById("date")
-
-    const hours           = `${date.getHours()}`.length < 2? `0${date.getHours()}` : date.getHours()
-    const minutes         = `${date.getMinutes()}`.length < 2? `0${date.getMinutes()}` : date.getMinutes()
-    const seconds         = `${date.getSeconds()}`.length < 2? `0${date.getSeconds()}` : date.getSeconds()
-    
-    const day             = `${date.getDay()}`.length < 2? `0${date.getDay()}` : date.getDay()
-    console.log(day)
-    const month           = `${date.getMonth()}`.length < 2? `0${date.getMonth()}` : date.getMonth()
-    const year            = date.getFullYear()
-    
-    boxHours.innerText    = hours
-    boxMinutes.innerText  = minutes
-    boxSeconds.innerText  = seconds
-
-    currentDate.innerText = `${day}/${month}/${year}`
+  currentDate.innerText = `${date}`.slice(0, 15);
 }
 
-setInterval(()=>{
-    getTime()
-}, 1000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setInterval(() => {
+  getTime();
+}, 1000);
